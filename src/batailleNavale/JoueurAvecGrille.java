914213591bf1faf40dbc587af870df1fed847922
CorrosiVz <153507119+CorrosiVz@users.noreply.github.com>
2023-12-12 17:20:25 +0100

    Create JoueurAvecGrille.java

---
+++ src/batailleNavale/JoueurAvecGrille.java
@@ -0,0 +1,20 @@
+public abstract class JoueurAvecGrille extends Joueur {
+  // Attributs :
+  private GrilleNavale grille;
+
+  // Constructeurs :
+  public JoueurAvecGrille(GrilleNavale g, String nom) {
+      super(nom);
+      this.grille = g;
+  }
+  public JoueurAvecGrille(GrilleNavale g) {
+      super();
+      this.grille = g;
+  }
+  public void defendre(Coordonnee c) {
+      //TO-DO
+  }
+  public boolean perd() {
+      return false;
+  }
+}
