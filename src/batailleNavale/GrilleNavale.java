6793df093de78ffc8737af2a1b5c1627b954948c
YuDZEN <144671117+YuDZEN@users.noreply.github.com>
2023-12-13 16:34:21 +0100

    Update GrilleNavale.java
    presque final version of GrilleNavale

--- src/batailleNavale/GrilleNavale.java
+++ src/batailleNavale/GrilleNavale.java
@@ -13,15 +13,15 @@ public class GrilleNavale {
     // Constructeurs
     public GrilleNavale(int taille, int[] taillesNavires) {
         this.taille = taille;
-        this.nbNavires = taillesNavires.length;
-        this.navires = new Navire[nbNavires];
+        this.nbNavires = 0;
+        this.navires = new Navire[taillesNavires.length];
         this.tirsRecus = new Coordonnee[taille * taille];
         this.nbTirsRecus=0;
     }
 
     public GrilleNavale(int taille, int nbNavires) {
         this.taille = taille;
-        this.nbNavires = nbNavires;
+        this.nbNavires = 0;
         this.navires = new Navire[nbNavires];
         this.tirsRecus = new Coordonnee[taille * taille];
         this.nbTirsRecus=0;
@@ -33,8 +33,11 @@ public class GrilleNavale {
         }
 
         public boolean ajouteNavire(Navire n) {
+            if (n == null) {
+                throw new IllegalArgumentException("Le navire est null.");
+            }
             // Vérifier si le navire chevauche, touche un autre navire déjà présent
-            for (int i = 0; i < nbNavires; i++) {
+            for (int i = 0; i < navires.length; i++) {
                 if (navires[i] != null && (navires[i].chevauche(n) || n.chevauche(navires[i]) || navires[i].touche(n) || n.touche(navires[i]))) {
                     return false;  // L'ajout est impossible car il y a chevauchement
                 }
@@ -44,9 +47,10 @@ public class GrilleNavale {
                 return false;  // L'ajout est impossible car le navire dépasse les limites
             }
             // Ajouter le navire à la grille
-            for (int i = 0; i < nbNavires; i++) {
+            for (int i = 0; i < navires.length; i++) {
                 if (navires[i] == null) {
                     navires[i] = n;
+                    nbNavires++;
                     return true;  // L'ajout a réussi
                 }
             }
@@ -63,18 +67,22 @@ public class GrilleNavale {
         for (int i = 1; i < taille + 1; i++) {
             grille[i][0] = Character.forDigit(i, 10);
         }
-        if (this.tirsRecus == null){
+        if (this.tirsRecus == null) {
 
-        }else {
+        } else {
             for (int i = 0; i < nbTirsRecus; i++) {
                 grille[tirsRecus[i].getLigne()][tirsRecus[i].getColonne()] = '◯';
             }
         }
-        for (int i = 0; i < navires.length; i++) {
-            if (navires[i].estVertical()) {
+
+
+        if (navires[0]!=null ) {
+
+        for (int i = 0; i < nbNavires; i++) {
+            if (navires[i] != null && navires[i].estVertical()) {
                 // vertical
-                int startLigne = navires[i].getDebut().getLigne()+1;
-                int startColonne = navires[i].getDebut().getColonne()+1;
+                int startLigne = navires[i].getDebut().getLigne() + 1;
+                int startColonne = navires[i].getDebut().getColonne() + 1;
                 int length = Math.abs(navires[i].getDebut().getLigne() - navires[i].getFin().getLigne()) + 1;
 
                 for (int j = 0; j < length; j++) {
@@ -92,8 +100,8 @@ public class GrilleNavale {
                 }
             } else {
                 // orientation horizontale
-                int startLigne = navires[i].getDebut().getLigne()+1;
-                int startColonne = navires[i].getDebut().getColonne()+1;
+                int startLigne = navires[i].getDebut().getLigne() + 1;
+                int startColonne = navires[i].getDebut().getColonne() + 1;
                 int length = Math.abs(navires[i].getDebut().getColonne() - navires[i].getFin().getColonne()) + 1;
 
                 for (int j = 0; j < length; j++) {
@@ -111,7 +119,7 @@ public class GrilleNavale {
                 }
             }
         }
-
+    }
         for (int i = 1; i < taille + 1; i++) {
             for (int j = 1; j < taille + 1; j++) {
                 if (grille[i][j] == '\u0000') {
@@ -121,6 +129,7 @@ public class GrilleNavale {
         }
         grille[0][0] = ' ';
         StringBuilder a = new StringBuilder();
+        a.append(" ");
         for (int i = 0; i < 1; i++) {
             for (int j = 0; j < taille + 1; j++) {
 
@@ -179,6 +188,7 @@ public class GrilleNavale {
                         break;
                     } else {
                         c = true;
+                        nbNavires++;
                     }
                 }
             }
@@ -264,21 +274,5 @@ public class GrilleNavale {
             }
         }
         return true;
-    }
-
-
-    public static void main(String[] args) {
-        int[] taillesNavires = {4, 5, 6,5,5,5};
-
-        GrilleNavale grilleNavale2 = new GrilleNavale(10, taillesNavires);
-        grilleNavale2.placementAuto(taillesNavires);
-        for (int i= 0; i<6; i++) {
-            System.out.println(grilleNavale2.navires[i]);
-        }
-        System.out.println(grilleNavale2.toString());
-        System.out.println(grilleNavale2.getTaille());
-
-
-
     }
 }
