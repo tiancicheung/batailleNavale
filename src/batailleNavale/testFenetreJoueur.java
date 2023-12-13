9c95522e1b07053baa5ee66c4894b5d557a28df5
tiancicheung <tiancicheung@163.com>
2023-12-13 20:52:36 +0100

    Merge remote-tracking branch 'origin/master'

--- src/batailleNavale/testFenetreJoueur.java
+++ src/batailleNavale/testFenetreJoueur.java
@@ -2,7 +2,7 @@ package batailleNavale;
 
 public class testFenetreJoueur {
     public static void main(String[] args) {
-        FenetreJoueur fenetre = new FenetreJoueur("Joueur", 10);
+        FenetreJoueur fenetre = new FenetreJoueur("Bataille Navale", 10);
         fenetre.setVisible(true);
     }
 }
