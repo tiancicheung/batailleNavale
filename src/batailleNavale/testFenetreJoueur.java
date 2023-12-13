6bcaf21d4b5401246f557a0018ecf905d76e9e2e
tiancicheung <tiancicheung@163.com>
2023-12-13 13:26:20 +0100

    class FenetreJoueur

---
+++ src/batailleNavale/testFenetreJoueur.java
@@ -0,0 +1,8 @@
+package batailleNavale;
+
+public class testFenetreJoueur {
+    public static void main(String[] args) {
+        FenetreJoueur fenetre = new FenetreJoueur("Joueur", 10);
+        fenetre.setVisible(true);
+    }
+}
