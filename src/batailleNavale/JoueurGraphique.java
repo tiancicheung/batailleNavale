db4b527bfd1d46c6fd81a989d26aef9077799db9
Anthony <ahmarqej@gmail.com>
2023-12-13 15:40:52 +0100

    anthony commit push test

--- src/batailleNavale/JoueurGraphique.java
+++ src/batailleNavale/JoueurGraphique.java
@@ -6,16 +6,19 @@ import javax.swing.*;
 public class JoueurGraphique extends JoueurAvecGrille {
     private GrilleGraphique grilleTirs;
     public JoueurGraphique(GrilleNavaleGraphique grilleDefense, GrilleGraphique grilleTirs, String nom) {
-
+        super(grilleDefense, nom);
+        this.grilleTirs = grilleTirs;
     }
     public JoueurGraphique(GrilleNavaleGraphique grilleDefense, GrilleGraphique grilleTirs) {
-
+        super(grilleDefense);
+        this.grilleTirs = grilleTirs;
     }
     public Coordonnee choixAttaque() {
-
+        return grilleTirs.getCoordonneeSelectionnee();
     }
     protected void retourDefense(Coordonnee c, int etat) {
-
+        // Affichage d'un JOptionPane lorsque le tir a touché ou coulé un navire,
+        // ou lorsque la partie est perdue.
     }
     protected void retourAttaque(Coordonnee c, int etat) {
         Color couleur = etat == A_L_EAU ? Color.BLUE : Color.RED;
