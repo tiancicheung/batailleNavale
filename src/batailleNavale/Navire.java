b78075e70f506ecbe3947c7d29bb664366008987
YuDZEN <chenyu.france@gmail.com>
2023-12-13 12:17:57 +0100

    a new new new version of GrilleNavale and Navires, the main change was adapted to PlacementAuto by adding a "break" to the for loop. the contientTo in Navires is also updated.

--- src/batailleNavale/Navire.java
+++ src/batailleNavale/Navire.java
@@ -1,95 +1,103 @@
 package batailleNavale;
 
 public class Navire {
-    // Attributs
+    //Attributes
     private Coordonnee debut;
     private Coordonnee fin;
     private Coordonnee[] partiesTouchees;
     private int nbTouchees;
 
-    // Constructeur
+
     public Navire(Coordonnee debut, int longueur, boolean estVertical) {
-        if (longueur <= 0) { // on vérifie que la longueur du navire est positive
+        if (longueur < 0) {
             throw new IllegalArgumentException("La longueur d'un navire ne peut être inférieure à 0, longueur = " + longueur);
         }
-        this.debut = debut; // on affecte les valeurs à debut
-
-        if (estVertical) // on affecte les valeurs à fin
-            fin = new Coordonnee(debut.getLigne() + longueur - 1, debut.getColonne()); // si le navire est vertical
+        this.debut = debut;
+        if (estVertical)
+            this.fin = new Coordonnee(debut.getLigne() + longueur - 1, debut.getColonne());
         else
-            fin = new Coordonnee(debut.getLigne(), debut.getColonne() + longueur - 1); // si le navire est horizontal
+            this.fin = new Coordonnee(debut.getLigne(), debut.getColonne() + longueur - 1);
+        this.partiesTouchees = new Coordonnee[longueur];
+        this.nbTouchees = 0;
 
-        partiesTouchees = new Coordonnee[longueur]; // on initialise le tableau partiesTouchees
-        nbTouchees = 0; // on initialise le nombre de parties touchées à 0
     }
 
-    // Methods
+
+    //Methods
     public boolean estVertical() {
-        return debut.getColonne() == fin.getColonne(); // on vérifie si le navire est vertical, si oui on retourne true
+        return debut.getColonne() == fin.getColonne();
     }
+
 
     public String toString() {
-        if (debut.getColonne() == fin.getColonne()) // on vérifie si le navire est vertical
-            return "Navire (" + debut + ", " + (fin.getLigne() - debut.getLigne() + 1) + ", " + "vertical)"; // si oui on retourne la chaîne correspondante
-        return "Navire (" + debut + ", " + (fin.getColonne() - debut.getColonne() + 1) + ", " + "horizontal)"; // si horizontal on retourne la chaîne correspondante
+        if (debut.getColonne() == fin.getColonne())
+            return "Navire (" + debut + ", " + (fin.getLigne() - debut.getLigne() + 1) + ", " + "vertical)";
+        return "Navire (" + debut + ", " + (fin.getColonne() - debut.getColonne() + 1) + ", " + "horizontal)";
     }
 
+
     public Coordonnee getDebut() {
-        return debut; // on retourne le début du navire
+        return this.debut;
     }
 
     public Coordonnee getFin() {
-        return fin; // on retourne la fin du navire
+        return this.fin;
     }
 
+
     public int getTouchLength() {
-        return nbTouchees; // on retourne le nombre de parties touchées
+        return this.nbTouchees;
     }
 
     public boolean contient(Coordonnee c) {
-        if (this.estVertical()) {
+        if (this.estVertical()){
             if (c.getColonne() == this.debut.getColonne() && c.getLigne() >= this.debut.getLigne() && c.getLigne() <= this.fin.getLigne())
                 return true;
-        } else if (!this.estVertical()) {
+        }else if (!this.estVertical()){
             if (c.getLigne() == this.debut.getLigne() && c.getColonne() >= this.debut.getColonne() && c.getColonne() <= this.fin.getColonne())
                 return true;
         }
         return false;
     }
 
+
     public boolean touche(Navire n) {
         // Si sur une ligne adjacente et sur même colonne & pas de chevauchement
-        if (n.getFin().getLigne() + 1 >= this.getDebut().getLigne() && this.getFin().getLigne() + 1 >= n.getDebut().getLigne()) // on vérifie si les navires sont sur une ligne adjacente
-            if (n.getFin().getColonne() >= this.getDebut().getColonne() && this.getFin().getColonne() >= n.getDebut().getColonne()) // si oui on vérifie si les navires sont sur la même colonne
-                if (!this.chevauche(n)) // on vérifie si les navires ne se chevauchent pas
-                    return true; // si oui on retourne true
+        if (n.getFin().getLigne() + 1 >= this.getDebut().getLigne() && this.getFin().getLigne() + 1 >= n.getDebut().getLigne())
+            if (n.getFin().getColonne() >= this.getDebut().getColonne() && this.getFin().getColonne() >= n.getDebut().getColonne())
+                if (this.chevauche(n) == false)
+                    return true;
+
 
         // Si sur une colonne adjacente et sur même ligne & pas de chevauchement
-        if (n.getFin().getColonne() + 1 >= this.getDebut().getColonne() && this.getFin().getColonne() + 1 >= n.getDebut().getColonne()) // on vérifie si les navires sont sur une colonne adjacente
-            if (n.getFin().getLigne() >= this.getDebut().getLigne() && this.getFin().getLigne() >= n.getDebut().getLigne()) // si oui on vérifie si les navires sont sur la même ligne
-                if (!this.chevauche(n)) // on vérifie si les navires ne se chevauchent pas
-                    return true; // si oui on retourne true
+        if (n.getFin().getColonne() + 1 >= this.getDebut().getColonne() && this.getFin().getColonne() + 1 >= n.getDebut().getColonne())
+            if (n.getFin().getLigne() >= this.getDebut().getLigne() && this.getFin().getLigne() >= n.getDebut().getLigne())
+                if (this.chevauche(n) == false)
+                    return true;
 
-        return false; // si les navires ne sont pas adjacents ou se chevauchent on retourne false
+
+        return false;
     }
 
+
     public boolean chevauche(Navire n) {
-        Coordonnee debutN = n.getDebut(); // on récupère le début et la fin du navire n
+
+        Coordonnee debutN = n.getDebut();
         Coordonnee finN = n.getFin();
-        for (int i = debutN.getLigne(); i <= finN.getLigne(); i++) { // on parcourt Navire debut -> fin pour voir si y a parti commun
+        for (int i = debutN.getLigne(); i <= finN.getLigne(); i++) {
             for (int j = debutN.getColonne(); j <= finN.getColonne(); j++) {
-                Coordonnee coord = new Coordonnee(i, j); // on crée une coordonnée
-                if (contient(coord)) { // si coord est dans Navire this
-                    return true; // on retourne true
+                Coordonnee coord = new Coordonnee(i, j); //on parcourt Navire debut -> fin pour voir si y a parti commun
+                if (contient(coord)) {
+                    return true; //y a parti commun, est collapsed
                 }
             }
         }
-        return false; // si on a pas trouvé de parti commun, on retourne false
+        return false;
     }
 
     //Retourne true si et seulement si this contient c. Dans ce cas, c est ajoutée aux parties touchées si nécessaire.
     public boolean recoitTir(Coordonnee c) {
-        if (partiesTouchees.length <= 0) {
+        if (partiesTouchees.length < 0) {
             throw new IllegalArgumentException("Le nombre de parties d'un navire ne peut être inférieur à 0, nombre de parties du navire = " + partiesTouchees.length);
         }
         if (contient(c)) {
@@ -105,12 +113,10 @@ public class Navire {
         return false;
     }
 
+    //Retourne true si et seulement si this a été touché par un tir en c. Pour Navire
     public boolean estTouche(Coordonnee c) {
-        if (partiesTouchees.length <= 0) {
-            throw new IllegalArgumentException("Le nombre de parties d'un navire ne peut être inférieur à 0, nombre de parties du navire = " + partiesTouchees.length);
-        }
         for (int i = 0; i < partiesTouchees.length; i++) {
-            if (partiesTouchees[i] != null && partiesTouchees[i].equals(c)) {
+            if (partiesTouchees[i] != null &&  partiesTouchees[i].equals(c)) {
                 return true; //si c appartient aux éléments dans partiesTouchees de Navire, ça va return true
             }
         }
@@ -118,27 +124,38 @@ public class Navire {
     }
 
     public void affichepartiesTouchees() {
-        if (partiesTouchees.length <= 0) { // on vérifie que le nombre de parties du navire est positif
+        if (partiesTouchees.length < 0) {
             throw new IllegalArgumentException("Le nombre de parties d'un navire ne peut être inférieur à 0, nombre de parties du navire = " + partiesTouchees.length);
         }
-        for (int i = 0; i < partiesTouchees.length; i++) {
-            if (partiesTouchees[i] != null) {
-                System.out.print(partiesTouchees[i] + " ");
-            }
-        }
+
+
+        for (int i = 0; i < partiesTouchees.length; i++)
+            System.out.print(partiesTouchees[i] + " ");
     }
 
     public boolean estTouche() {
         if (nbTouchees < 0) {
             throw new IllegalArgumentException("Le nombre de fois que le navire est touché ne peut être inférieur à 0, nbTouchees = " + nbTouchees);
         }
-        return nbTouchees > 0; // si nbTouchees > 0, ça veut dire que le navire est touché
+
+
+        if (nbTouchees > 0)
+            return true;
+        return false;
+
     }
 
     public boolean estCoule() {
         if (nbTouchees < 0) {
             throw new IllegalArgumentException("Le nombre de fois que le navire est touché ne peut être inférieur à 0, nbTouchees = " + nbTouchees);
         }
-        return nbTouchees == partiesTouchees.length; // si nbTouchees == partiesTouchees.length, ça veut dire que le navire est coulé
+        if (nbTouchees > partiesTouchees.length) {
+            throw new IllegalArgumentException("Le nombre de fois que le navire est touché ne peut pas être supérieur au nombre de parties du navire, nbTouchees = " + nbTouchees + " , nombre de parties du navire = " + partiesTouchees.length);
+        }
+
+
+        if (nbTouchees == partiesTouchees.length)
+            return true; // si partiesTouchees [] est tout remplit, navire est mort
+        return false;
     }
 }
